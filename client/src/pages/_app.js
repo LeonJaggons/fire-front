2b063aa7ebadd09e34e9f1ff1a0e57ee51e80ce6
Leon Jaggon <leonojaggon@gmail.com>
2023-10-12 23:38:17 -0700

    Api + other app foundation

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,22 +1,86 @@
+import "../firebase/firebase-init";
 import { Link } from "@chakra-ui/next-js";
-import { Box, ChakraProvider, HStack, Heading } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    ChakraProvider,
+    Collapse,
+    HStack,
+    Heading,
+    Icon,
+    Select,
+    useDisclosure,
+} from "@chakra-ui/react";
+import {
+    Provider as ReduxProvider,
+    useDispatch,
+    useSelector,
+} from "react-redux";
+
+import { MdCampaign, MdSpeaker } from "react-icons/md";
+import { GiAwareness } from "react-icons/gi";
 import "@styles/globals.scss";
 import NextLink from "next/link";
+import { store } from "src/redux/store";
+import { useEffect } from "react";
+import {
+    loadAllConflicts,
+    loadConflictEventTypes,
+    streamConflictEvents,
+} from "src/services/conflictService";
+import {
+    setSelectedConflict,
+    toggleClickToReportMode,
+} from "src/redux/slices/conflictSlice";
+import { find } from "lodash";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ChakraProvider>
+        <Providers>
             <AppContainer>
                 <AppHeader />
                 <AppContent>
                     <Component {...pageProps} />
                 </AppContent>
             </AppContainer>
-        </ChakraProvider>
+        </Providers>
     );
 }
 
+const Providers = ({ children }) => {
+    return (
+        <ChakraProvider>
+            <ReduxProvider store={store}>{children}</ReduxProvider>
+        </ChakraProvider>
+    );
+};
+
 const AppContainer = ({ children }) => {
+    const conflictEvents = useSelector(
+        (state) => state.conflict.conflictEvents
+    );
+    const selectedConflict = useSelector(
+        (state) => state.conflict.selectedConflict
+    );
+    const onFirstLoad = async () => {
+        await loadAllConflicts();
+        await loadConflictEventTypes();
+    };
+    useEffect(() => {
+        onFirstLoad();
+    }, []);
+    useEffect(() => {
+        let unsub;
+        if (selectedConflict) {
+            unsub = streamConflictEvents(selectedConflict.id);
+        }
+        return () => {
+            unsub && unsub();
+        };
+    }, [selectedConflict]);
+    useEffect(() => {
+        console.log(conflictEvents);
+    }, [conflictEvents]);
     return <Box id={"app-container"}>{children}</Box>;
 };
 const AppContent = ({ children }) => {
@@ -24,26 +88,111 @@ const AppContent = ({ children }) => {
 };
 
 const AppHeader = () => {
+    const appName = useSelector((state) => state.conflict.appName);
+    const clickToReportMode = useSelector(
+        (state) => state.conflict.clickToReportMode
+    );
     return (
-        <HStack id={"app-header"}>
-            <Heading size={"md"}>Fire Front</Heading>
-            <AppMenu />
-        </HStack>
+        <Collapse in={!clickToReportMode}>
+            <HStack
+                id={"app-header"}
+                shadow={"md"}
+                spacing={"80px"}
+                h={"64px"}
+                bg={"black"}
+                color={"white"}
+            >
+                {/* <Heading size={"md"}`o>{appName}</Heading> */}
+
+                <HStack>
+                    <Icon as={GiAwareness} boxSize={"30px"} mr={2} />
+                    <ConflictSelect />
+                </HStack>
+                <Box flex={1} />
+                <AppMenu />
+            </HStack>
+        </Collapse>
+    );
+};
+
+const ConflictSelect = () => {
+    const conflicts = useSelector((state) => state.conflict.conflicts);
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        const conflict = find(conflicts, (c) => c.id === e.target.value);
+
+        dispatch(setSelectedConflict(null));
+        dispatch(setSelectedConflict(conflict));
+    };
+    return (
+        <Select
+            variant={"unstyled"}
+            fontSize={"28px"}
+            fontWeight={"800"}
+            onChange={handleChange}
+            flex={"1 1 0px"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            borderBottomColor={"white"}
+            _focus={{ borderColor: "white" }}
+            _active={{ borderColor: "white" }}
+        >
+            {conflicts?.map((c) => (
+                <option value={c.id}>{c.name}</option>
+            ))}
+        </Select>
     );
 };
 
 const AppMenu = () => {
     return (
         <HStack id={"app-menu"} spacing={8}>
+            <ReportButton />
             <AppMenuItem>Map</AppMenuItem>
-            <AppMenuItem>Conflict</AppMenuItem>
+
+            <AppMenuItem href={"/conflict"}>Conflict</AppMenuItem>
         </HStack>
     );
 };
 
-const AppMenuItem = ({ children }) => {
+const ReportButton = () => {
+    const { isOpen, onClose, onToggle } = useDisclosure();
+    const clickToReportMode = useSelector(
+        (state) => state.conflict.clickToReportMode
+    );
+
+    const dispatch = useDispatch();
+    const handleClick = () => {
+        dispatch(toggleClickToReportMode());
+    };
+
+    useEffect(() => {
+        console.log(clickToReportMode);
+    }, [clickToReportMode]);
     return (
-        <Link className="app-menu-item" href={"/"} as={NextLink}>
+        <>
+            <Button
+                onClick={handleClick}
+                fontWeight={400}
+                fontSize={"14px"}
+                variant={"ghost"}
+                color={!clickToReportMode ? "black" : "white"}
+                bg={!clickToReportMode ? "white" : "black"}
+                colorScheme={!clickToReportMode ? "blackAlpha" : "whiteAlpha"}
+                _hover={{
+                    bg: !clickToReportMode ? "gray.200" : "gray.800",
+                }}
+                rightIcon={<Icon as={MdCampaign} boxSize={"24px"} />}
+            >
+                Report Event
+            </Button>
+        </>
+    );
+};
+const AppMenuItem = ({ children, href }) => {
+    return (
+        <Link className="app-menu-item" href={href ?? "/"} as={NextLink}>
             {children}
         </Link>
     );
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,43 @@
 import Head from "next/head";
 
-import Map from "../components/Map/Map";
+import { ConflictMap } from "../components/ConflictMap";
+import { useDispatch, useSelector } from "react-redux";
+import { Alert } from "@chakra-ui/alert";
+import { Collapse } from "@chakra-ui/transition";
+import { Box, Center, HStack, Heading, Text, VStack } from "@chakra-ui/layout";
+import { Icon } from "@chakra-ui/icon";
+import {
+    MdArrowUpward,
+    MdCampaign,
+    MdFlight,
+    MdLocalFireDepartment,
+    MdOutlineArrowDownward,
+    MdOutlineArrowUpward,
+} from "react-icons/md";
+import {
+    PiArrowCircleDown,
+    PiArrowCircleDownFill,
+    PiArrowCircleUp,
+    PiArrowCircleUpFill,
+} from "react-icons/pi";
+import { Button, IconButton } from "@chakra-ui/button";
+import { toggleClickToReportMode } from "src/redux/slices/conflictSlice";
+import { Tag } from "@chakra-ui/tag";
+import { useState } from "react";
 
-const DEFAULT_CENTER = [38.907132, -77.036546];
+export const DEFAULT_CENTER = [38.907132, -77.036546];
 
 export default function Home() {
+    const selectedConflict = useSelector(
+        (state) => state.conflict.selectedConflict
+    );
+    const clickToReportMode = useSelector(
+        (state) => state.conflict.clickToReportMode
+    );
+    const dispatch = useDispatch();
+    const cancelClickToReport = () => {
+        dispatch(toggleClickToReportMode());
+    };
     return (
         <>
             <Head>
@@ -15,26 +48,156 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Collapse in={clickToReportMode} bg={"black"}>
+                <Alert bg={"black"} color={"white"} py={2} h={"64px"}>
+                    <HStack w={"full"}>
+                        <HStack flex={1}>
+                            <Icon
+                                as={MdCampaign}
+                                color={"white"}
+                                boxSize={"23px"}
+                            />
 
-            <Map
-                style={{ width: "100%", height: "100%" }}
-                center={DEFAULT_CENTER}
-                zoom={12}
+                            <Text color={"white"} fontSize={"sm"}>
+                                Click anywhere on the map to report an event for
+                                the {selectedConflict?.name}.
+                            </Text>
+                        </HStack>
+                        <Button
+                            p={1}
+                            size={"sm"}
+                            onClick={cancelClickToReport}
+                            variant={"ghost"}
+                            colorScheme={"whiteAlpha"}
+                            color={"white"}
+                        >
+                            Cancel
+                        </Button>
+                    </HStack>
+                </Alert>
+            </Collapse>
+
+            <HStack
+                spacing={0}
+                h={"calc(100vh - 64px)"}
+                // border={clickToReportMode && "8px solid white"}
             >
-                {({ TileLayer, Marker, Popup }) => (
-                    <>
-                        <TileLayer
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        />
-                        <Marker position={DEFAULT_CENTER}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                        </Marker>
-                    </>
-                )}
-            </Map>
+                <ConflictMap />
+                <ConflictList />
+            </HStack>
         </>
     );
 }
+
+const ConflictList = () => {
+    const clickToReportMode = useSelector(
+        (state) => state.conflict.clickToReportMode
+    );
+    const conflictEvents = useSelector(
+        (state) => state.conflict.conflictEvents
+    );
+    return (
+        <VStack
+            // bg={"black"}
+            h={"full"}
+            alignItems={"flex-start"}
+            minW={"25vw"}
+            bg={"gray.100"}
+        >
+            <Box
+                p={4}
+                bg={true ? "red.500" : "#333333"}
+                borderBottom={"1px solid rgba(0,0,0,.1)"}
+                w={"full"}
+                color={"white"}
+            >
+                <Heading size={"md"} color={"white"} mb={1}>
+                    Conflict Events
+                </Heading>
+                <Text fontSize={12}>Last updated</Text>
+            </Box>
+
+            {conflictEvents?.map((c) => (
+                <ConflictEvent c={c} />
+            ))}
+        </VStack>
+    );
+};
+
+const ConflictEvent = ({ c }) => {
+    const [isUpvoted, setIsUpvoted] = useState();
+    const getFFIcon = (name) => {
+        switch (name) {
+            case "Airstrike/Bombing":
+                return MdFlight;
+            case "Firefight/Shooting":
+                return MdLocalFireDepartment;
+            case "Leader Announcement":
+                return MdCampaign;
+        }
+        return null;
+    };
+    return (
+        <Box
+            shadow={"sm"}
+            _hover={{ shadow: "md" }}
+            w={"full"}
+            p={4}
+            py={2}
+            cursor={"pointer"}
+        >
+            <VStack w={"full"}>
+                <HStack spacing={4} w={"full"}>
+                    <Center
+                        boxSize={"50px"}
+                        w={"50px"}
+                        bg={"red.500"}
+                        borderRadius={"25px"}
+                    >
+                        <Icon
+                            as={getFFIcon(c.conflictEventType.name)}
+                            color={"white"}
+                            boxSize={"34px"}
+                        />
+                    </Center>
+                    <VStack
+                        w={"full"}
+                        alignItems={"flex-start"}
+                        flex={1}
+                        spacing={1}
+                    >
+                        <Heading size={"sm"}>{c.title}</Heading>
+
+                        <Text fontSize={"sm"}>{c.description}</Text>
+                    </VStack>
+                    <VStack spacing={0}>
+                        <IconButton
+                            variant={"ghost"}
+                            icon={
+                                <Icon
+                                    as={
+                                        isUpvoted && isUpvoted === true
+                                            ? PiArrowCircleUpFill
+                                            : PiArrowCircleUp
+                                    }
+                                />
+                            }
+                        />
+                        <IconButton
+                            variant={"ghost"}
+                            icon={
+                                <Icon
+                                    as={
+                                        isUpvoted && isUpvoted === false
+                                            ? PiArrowCircleDownFill
+                                            : PiArrowCircleDown
+                                    }
+                                />
+                            }
+                        />
+                    </VStack>
+                </HStack>
+            </VStack>
+        </Box>
+    );
+};
