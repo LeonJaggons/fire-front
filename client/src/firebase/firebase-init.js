fec56e318c8eb803c09d71ad8448358c72c08b88
Leon Jaggon <leonojaggon@gmail.com>
2023-10-12 23:38:06 -0700

    Conflig + firebase

---
+++ client/src/firebase/firebase-init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFEoB5H8HyyW2M62l0BzuSxFM8mvrqSzo",
+    authDomain: "firefront-c3abb.firebaseapp.com",
+    projectId: "firefront-c3abb",
+    storageBucket: "firefront-c3abb.appspot.com",
+    messagingSenderId: "862065968550",
+    appId: "1:862065968550:web:24dd70e15e5fc094e57b67",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const fireStore = getFirestore(app);
+export const fireAuth = getAuth(app);
