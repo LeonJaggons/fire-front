2fdae5dd29e1088a96e805429de5d3f63665a365
Leon Jaggon <leonojaggon@gmail.com>
2023-10-12 23:37:31 -0700

    Add conflict state +  database pulls

---
+++ client/src/redux/slices/conflictSlice.js
@@ -0,0 +1,90 @@
+import { createSlice } from "@reduxjs/toolkit";
+const EmptyConflictEvent = {
+    conflictEventTypeId: "",
+    title: "",
+    description: "",
+    source: "",
+};
+const initialState = {
+    appName: "FireFront",
+    conflictEventTypes: null,
+    selectedConflict: null,
+    conflicts: null,
+    reportLocation: null,
+    clickToReportMode: false,
+    newConflictEvent: EmptyConflictEvent,
+    conflictEvents: [],
+};
+export const conflictSlice = createSlice({
+    name: "conflict",
+    initialState,
+    reducers: {
+        setConflicts: (state, action) => {
+            state.conflicts = [...action.payload];
+        },
+
+        setSelectedConflict: (state, action) => {
+            console.log(action.payload);
+            if (action.payload) {
+                state.selectedConflict = { ...action.payload };
+            } else {
+                state.selectedConflict = {};
+            }
+        },
+        setConflictEventTypes: (state, action) => {
+            state.conflictEventTypes = action.payload;
+        },
+        setReportLocation: (state, action) => {
+            state.reportLocation = action.payload;
+        },
+        toggleClickToReportMode: (state) => {
+            state.clickToReportMode = !state.clickToReportMode;
+        },
+        updateNewConflictEventTitle: (state, action) => {
+            state.newConflictEvent = {
+                ...state.newConflictEvent,
+                title: action.payload,
+            };
+        },
+        updateNewConflictEventDescription: (state, action) => {
+            state.newConflictEvent = {
+                ...state.newConflictEvent,
+                description: action.payload,
+            };
+        },
+        updateNewConflictEventSource: (state, action) => {
+            state.newConflictEvent = {
+                ...state.newConflictEvent,
+                source: action.payload,
+            };
+        },
+        updateNewConflictEventTypeId: (state, action) => {
+            state.newConflictEvent = {
+                ...state.newConflictEvent,
+                conflictEventTypeId: action.payload,
+            };
+        },
+        clearNewConflict: (state) => {
+            state.newConflictEvent = EmptyConflictEvent;
+        },
+        setConflictEvents: (state, action) => {
+            state.conflictEvents = action.payload;
+        },
+    },
+});
+
+export const {
+    setConflicts,
+    setSelectedConflict,
+    setReportLocation,
+    setConflictEventTypes,
+    toggleClickToReportMode,
+    updateNewConflictEventTitle,
+    updateNewConflictEventDescription,
+    updateNewConflictEventSource,
+    updateNewConflictEventTypeId,
+    clearNewConflict,
+    setConflictEvents,
+} = conflictSlice.actions;
+
+export default conflictSlice.reducer;
---
+++ client/src/services/conflictService.js
@@ -0,0 +1,99 @@
+import {
+    addDoc,
+    collection,
+    getDocs,
+    onSnapshot,
+    query,
+    where,
+} from "@firebase/firestore";
+import { find } from "lodash";
+import { fireStore } from "src/firebase/firebase-init";
+import {
+    setConflictEventTypes,
+    setConflictEvents,
+    setConflicts,
+    setReportLocation,
+    setSelectedConflict,
+} from "src/redux/slices/conflictSlice";
+import { store } from "src/redux/store";
+
+const conflictCollection = collection(fireStore, "conflict");
+const conflictEventCollection = collection(fireStore, "conflictEvent");
+const conflictEventTypeCollection = collection(fireStore, "conflictEventType");
+
+export const getAllConflicts = async () => {
+    const conflictsSnap = await getDocs(conflictCollection);
+    const conflicts = conflictsSnap.docs.map((c) => {
+        const conflictId = c.id;
+        const conflictData = c.data();
+        return {
+            id: conflictId,
+            ...conflictData,
+            startDate: conflictData.startDate
+                ? conflictData.startDate.toDate()
+                : null,
+            endDate: conflictData.endDate
+                ? conflictData.endDate.toDate()
+                : null,
+        };
+    });
+    return conflicts;
+};
+
+export const loadAllConflicts = async () => {
+    const conflicts = await getAllConflicts();
+    if (conflicts.length > 0) {
+        store.dispatch(setConflicts(conflicts));
+        store.dispatch(setSelectedConflict(conflicts[0]));
+    }
+};
+export const getAllConflictEventTypes = async () => {
+    const conflictEventTypeSnap = await getDocs(conflictEventTypeCollection);
+    const conflictEventTypes = conflictEventTypeSnap.docs.map((c) => {
+        return { id: c.id, ...c.data() };
+    });
+    return conflictEventTypes;
+};
+
+export const loadConflictEventTypes = async () => {
+    const conflictEventTypes = await getAllConflictEventTypes();
+    store.dispatch(setConflictEventTypes(conflictEventTypes));
+};
+export const publishConflictEvent = async () => {
+    const selectedConflict = store.getState().conflict.selectedConflict;
+    const newConflictEvent = store.getState().conflict.newConflictEvent;
+    const reportLocation = store.getState().conflict.reportLocation;
+
+    const conflictEvent = {
+        ...newConflictEvent,
+        conflictId: selectedConflict.id,
+        latitude: reportLocation[0],
+        longitude: reportLocation[1],
+    };
+
+    await addDoc(conflictEventCollection, conflictEvent);
+    store.dispatch(setReportLocation(null));
+};
+
+export const streamConflictEvents = (conflictId) => {
+    const conflictEventQry = query(
+        conflictEventCollection,
+        where("conflictId", "==", conflictId)
+    );
+    const unsub = onSnapshot(conflictEventQry, (snap) => {
+        let conflictEvents = [];
+        for (let conflictDoc of snap.docs) {
+            const conflictEventData = conflictDoc.data();
+            const conflictEvent = {
+                ...conflictEventData,
+                conflictEventType: find(
+                    store.getState().conflict.conflictEventTypes,
+                    (o) => o.id === conflictEventData.conflictEventTypeId
+                ),
+            };
+            conflictEvents.push(conflictEvent);
+        }
+        store.dispatch(setConflictEvents(conflictEvents));
+    });
+    return unsub;
+};
